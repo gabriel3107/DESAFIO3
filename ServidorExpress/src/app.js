866624e574e76008c5f1625ea9209a37e72e99a2
Gabriel Escarate Prieto <gabrielescarate33@gmail.com>
2023-09-27 00:50:21 -0500

    desafio 3 actualizado

--- ServidorExpress/src/app.js
+++ ServidorExpress/src/app.js
@@ -9,28 +9,11 @@ const products = [
     { id: 3, title: 'arroz', description: 'abarrotes', price: 8, thumbnail: '',code: 'prod003', stock: '20' }
 ];
 
-//Vamos a construir nuestro primer endpoint o servicio
-//Vamos a revisar la petición http de tipo GET
-// app.get('/saludo', (req, res) => {
-//     res.send('Hola a todos este es mi primer endpoint desde express');
-// });
+app.get('/bienvenida', (req, res) => {
+    res.send(`<h1 style="color:blue">Bienvenido a mi primer servidor de express</h1>`)
+});
 
-// app.get('/bienvenida', (req, res) => {
-//     res.send(`<h1 style="color:blue">Bienvenido a mi primer servidor de express</h1>`)
-// });
 
-// //Servicio usando un path param
-// app.get('/unparametro/:nombre', (req, res) => {
-//     res.send(`Bienvenido ${req.params.nombre}`);
-// });
-
-// app.get('/dosparametros/:nombre/:apellido', (req, res) => {
-//     res.send(`Bienvenido ${req.params.nombre} ${req.params.apellido}`);
-// });
-
-//Debemos construir un servicio que me permita obtener un usuario por su id
-//El identificador del usuario que vamos a buscar lo obtenemos mendiante un path param
-//Todo lo que envieemos como path param siempre es una cadena de texto
 app.get('/productos/:id', (req, res) => {
     const producId = Number(req.params.id);
     const produc = products.find(p => p.id === producId);
@@ -39,22 +22,13 @@ app.get('/productos/:id', (req, res) => {
 });
 
 app.get('/productosquery', (req, res) => {
-    // req.query {
-    //     edad: 28,
-    //     genero: 'M',
-    //     nombre: 'Alex'
-    // }
     const queryParams = req.query;
     res.send(queryParams);
 });
 
 app.get('/productos', (req, res) => {
-    //Obtenemos el genero del query param
     const producto = req.query.description;
-    //Siempre importante hacer validaciones
-    //En el caso de que no llegue el genero o el genero sea diferente de M o F retornamos los usuarios sin filtrar
     if(!description||(description!=='fruta'&&description!=='golosina'&&description!=='abarrotes')) return res.send({products});
-    //filtramos los usuarios por genero
     const filteredproduc = products.filter(produc=>produc.description===description);
     res.send({productos: filteredproduc});
 });
--- managers/app.js
+++ managers/app.js
@@ -3,21 +3,17 @@ import ProductManager from './ProductManager.js';
 
 const app = express();
 
-//Creamos la instancia de la clase
 const productManager = new ProductManager('./files/productos.json');
 
 app.use(express.urlencoded({extended: true}));
 
 
-//Ruta /products tipo get app.get llamar al método getAll de la clase ProductManager para esto hay que instanciar la clase
 app.get('/products', async (req, res) => {
     const products = await productManager.getAll();
     res.send(products);
 })
 
-//Ruta /products/:pid tipo app.get donde debemos llamar al metodo getById de la clae ProductManager usar la instancia de la clase ya creada
 
-//Ejmplo de async await llamado a la clase ProductMnager
 app.get('/', async (req,res)=> {
     const products = await productManager.getAll();
     res.send({products});
